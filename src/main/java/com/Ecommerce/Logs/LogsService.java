9e33ff53d502d275c5c39069e875c9debc0381f6
medrassa ntic <medrassachanuwu@gmail.com>
2022-05-26 18:09:06 +0100

    adding a endpoint to add log

--- src/main/java/com/Ecommerce/Logs/LogsService.java
+++ src/main/java/com/Ecommerce/Logs/LogsService.java
@@ -1,13 +1,13 @@
 package com.Ecommerce.Logs;
 
-import com.Ecommerce.confige.ApplicationContextHolder;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
-
 import com.Ecommerce.User.User;
+import com.Ecommerce.User.UserRepo;
+import lombok.AllArgsConstructor;
 
 import lombok.extern.java.Log;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,7 +15,21 @@ import org.springframework.stereotype.Service;
 public class LogsService {
 
     private LogsRepo logsRepo;
-    public void addLog(Logs logs){
-            logsRepo.save(logs);
-   }
+    private UserRepo userRepo;
+
+    public ResponseEntity<?> addLog(Authentication authentication, Logs logs) {
+        User user = userRepo.findUserByEmail(authentication.getPrincipal().toString());
+        if (user != null) {
+            logs.setUser(user);
+            if (logs.getRefreshToken() != null) {
+                Logs saved = logsRepo.save(logs);
+                return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(saved);
+            } else {
+                return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body("please add the refrechToken");
+            }
+
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
