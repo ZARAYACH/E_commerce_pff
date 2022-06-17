730ebec4cb4bba99b042195d74d498171560e8eb
MohammedChbani <mohammed.chbani@cprofile.nl>
2022-06-18 00:34:48 +0100

    fixing a bug

--- src/main/java/com/Ecommerce/Category/Category.java
+++ src/main/java/com/Ecommerce/Category/Category.java
@@ -1,5 +1,7 @@
 package com.Ecommerce.Category;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
